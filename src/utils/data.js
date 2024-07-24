2e7a157839bead5aa5e9ed9c9195bfabf0fee76f
Santu-Hansariafood <santu@hansariafood.com>
2024-07-24 11:39:57 +0530

    added the full code

---
+++ src/utils/data.js
@@ -0,0 +1,182 @@
+import {
+  IoLogoJavascript,
+  IoLogoHtml5,
+  IoLogoCss3,
+  IoLogoReact,
+  IoLogoNodejs,
+} from "react-icons/io5";
+
+import { RiTailwindCssFill } from "react-icons/ri";
+import {
+  SiMongodb,
+  SiExpress,
+  SiGit,
+  SiTypescript,
+  SiNextdotjs,
+} from "react-icons/si";
+
+export const PROFILE_DATA = {
+  name: "Santu De",
+  tagline: `As an experienced MERN stack developer, I am actively seeking
+opportunities to apply my skills and drive innovation within a progressive
+organization. I am dedicated to enhancing productivity, fostering
+individual growth, and delivering high-quality web solutions.`,
+  jobTitle: "Full Stack Developer",
+  location: "Kolkata, WestBengal, India",
+  yearsOfExperience: "3+",
+  skills: [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "NodeJS",
+    "ExpressJs",
+    "MongoDB",
+    "MERN Stack",
+    "NextJs",
+    "TypeScript",
+    "Git",
+  ],
+  email: "santude1997@gmail.com",
+  phone: "9064527639",
+  website: "www.santude.com",
+  socialLinks: {
+    github: "https://github.com/santude1997",
+    linkedin: "https://www.linkedin.com/in/santu-de-812571158/",
+    twitter: "https://twitter.com/santude1997",
+    facebook: "https://www.facebook.com/santude",
+    instagram: "https://www.instagram.com/santude",
+    whatsapp: "https://wa.me/7029481930",
+  },
+  resume: "https://drive.google.com/file/d/1Z6X4J5Y0",
+};
+
+export const ABOUT_ME_DATA = {
+  introduction:
+    "As a seasoned software developer with over three years of experience, I have honed my skills and expertise in the ever-evolving world of technology. My journey began with a Master of Computer Applications (MCA) from the esteemed Future Institute of Engineering and Management, where I laid the foundation for my technical proficiency and innovative thinking.",
+  background:
+    "In the past three years, I have immersed myself in the MERN (MongoDB, Express.js, React, Node.js) stack, successfully delivering robust and scalable web solutions. My portfolio boasts over ten diverse projects completed for various companies, each tailored to meet unique business needs and challenges. These projects have not only sharpened my technical acumen but also enriched my problem-solving capabilities and project management skills.",
+  skills:
+    "My passion for technology extends beyond professional projects. I have an insatiable curiosity for learning new things, constantly exploring the latest trends and advancements in web development. This drive for knowledge fuels my ability to create comprehensive and fully functional websites, ensuring that they are not only aesthetically pleasing but also highly efficient and user-friendly.",
+  projects: "Completes 10+ projects for different compnay",
+  interests: "My Interest is to learning new Things and full website",
+  careerGoal:
+    "My ultimate career goal is to craft high-quality web solutions that stand out for their innovation, reliability, and performance. I am dedicated to enhancing productivity within any organization I join, fostering individual and collective growth, and pushing the boundaries of what is possible in web development. Through continuous learning and application of cutting-edge technologies, I aspire to make significant contributions to the tech industry, driving innovation and excellence in every project I undertake.",
+
+  stats: {
+    yearsOfExperience: "3+",
+    projects: "10+",
+    clients: "5+",
+  },
+};
+
+export const WORK_EXPERIENCE = [
+  {
+    id: "01",
+    company: "Hansaria Food Private Limited",
+    postion: "Software Engineer",
+    duration: "Octber,2023- Present",
+    description:
+      "As the lead developer overseeing a team tasked with developing a comprehensive company software solution, our primary objective is to deliver a robust platform using React for frontend, Express and Node.js for server-side logic, and MongoDB for the database. Central to our efforts is the creation of an efficient admin panel, providing administrators with comprehensive control over system operations, user permissions, and settings. Role-based access control ensures that employees have tailored access to functionalities relevant to their roles, enhancing security and promoting efficiency.",
+  },
+  {
+    id: "02",
+    company: "DataSpace Security Private Limited",
+    postion: "Software Engineer",
+    duration: "September, 2023 - October,2023",
+    description:
+      "During my tenure as a Software Developer at Dataspace Security Pvt Ltd, I played a crucial role in the development of a Learning Management System. We leveraged cutting-edge technology, utilizing Next.js for the frontend, Node.js for the backend, and MongoDB for data storage. My responsibilities encompassed designing and maintaining the homepage, optimizing the marketing page, and ensuring the seamless development of the platform. ",
+  },
+  {
+    id: "03",
+    company: "RWS Moravia India Private Limited",
+    postion: "Software Engineer",
+    duration: "Auguest, 2021 - Augest, 2023",
+    description:
+      "During my time, as a Software Developer at RWS Moravia India I had a role in the development of a Business Standard news portal. We utilized cutting edge technology employing Next.js for the frontend Node.js for the backend and MongoDB, for data storage. My responsibilities included designing and maintaining the homepage optimizing the marketing page and ensuring development. Alongside this I also managed content and facilitated client interactions to ensure that our project met their expectations. This position highlighted the importance of collaboration having a technology stack and delivering a top notch web platform.",
+  },
+  {
+    id: "04",
+    company: "Startup ACE",
+    postion: "Software Engineer(Intern)",
+    duration: "Feburiy , 2021 - Augest, 2021",
+    description:
+      "The project, for managing hospitals took an approach beginning with research and analysis moving on to design and implementation and finally achieving a successful deployment on Netlify. Noteworthy components include managing patients and staff controlling inventory handling finances and ensuring user authentication. The fact that the project was completed in three months highlights project management. Additionally the focus on creating a user website design improves accessibility, for healthcare professionals",
+  },
+];
+
+export const SKILLS = [
+  {
+    id: "01",
+    icon: IoLogoHtml5,
+    title: "HTML",
+    comment: `HTML stands for Hyper Text Markup Language. HTML is the standard markup language for creating Web pages.`,
+  },
+  {
+    id: "02",
+    icon: IoLogoCss3,
+    title: "CSS",
+    comment: `The full form of CSS is Cascading Style Sheets. It is enabled by browsers and is specifically designed to distinguish the document's content from the report's appearance.`,
+  },
+  {
+    id: "03",
+    icon: IoLogoJavascript,
+    title: "Javascript",
+    comment: `JS stands for JavaScript. It is a text-based, lightweight, cross-platform, and interpreted scripting programming language. This language is very popular for developing web pages.`,
+  },
+  {
+    id: "04",
+    icon: IoLogoReact,
+    title: "React",
+    comment: `React is a powerful JavaScript library for building dynamic and interactive user interfaces (UIs). It is developed by Facebook. React is known for its component-based architecture which allows you to create reusable UI elements, making complex web applications easier to manage and maintain.`,
+  },
+  {
+    id: "05",
+    icon: IoLogoNodejs,
+    title: "NodeJs",
+    comment: `Node.js is a cross-platform, open-source JavaScript runtime environment that can run on Windows, Linux, Unix, macOS, and more. Node.js runs on the V8 JavaScript engine, and executes JavaScript code outside a web browser.`,
+  },
+  {
+    id: "06",
+    icon: SiExpress,
+    title: "ExpressJs",
+    comment: `Express. js is the most popular web framework for Node. js. It is designed for building web applications and APIs and has been called the de facto standard server framework for Node. js.`,
+  },
+  {
+    id: "07",
+    icon: SiMongodb,
+    title: "MongoDB",
+    comment: `The name of the database was derived from the word humongous to represent the idea of supporting large amounts of data. Merriman and Horowitz helped form 10Gen Inc. in 2007 to commercialize MongoDB and related software. The company was renamed MongoDB Inc.`,
+  },
+  {
+    id: "08",
+    icon: IoLogoJavascript,
+    title: "MERN Stack",
+    comment: `The MERN stack is a collection of technologies that help developers build robust and scalable web applications using JavaScript. The acronym “MERN” stands for MongoDB, Express, React, and Node. js, with each component playing a role in the development process.`,
+  },
+  {
+    id: "09",
+    icon: SiNextdotjs,
+    title: "NextJS",
+    comment: `Next.js is a React framework that gives you building blocks to create web applications. By framework, we mean Next.js handles the tooling and configuration needed for React, and provides additional structure, features, and optimizations for your application.`,
+  },
+  {
+    id: "10",
+    icon: SiTypescript,
+    title: "TypeScript",
+    comment: `TypeScript is JavaScript with syntax for types.
+TypeScript is a strongly typed programming language that builds on JavaScript, giving you better tooling at any scale.`,
+  },
+  {
+    id: "11",
+    icon: SiGit,
+    title: "Git",
+    comment: `The name "git" was given by Linus Torvalds when he wrote the very first version. He described the tool as "the stupid content tracker" and the name as (depending on your mood): random three-letter combination that is pronounceable, and not actually used by any common UNIX command.`,
+  },
+  {
+    id: "12",
+    icon: RiTailwindCssFill,
+    title: "TailwindCSS",
+    comment: `As the name suggests, Tailwind CSS is a CSS framework. It does not contain JavaScript code and does not offer solutions related to it. However, that doesn't mean we shouldn't use JS in Tailwind. Quite the opposite.`,
+  },
+];
